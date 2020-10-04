a918a46f89d95e8423f8e1f13d950912e7052131
Sopanat <sopanus2014@gmail.com>
2020-10-04 22:50:25 +0700

    Refactor  : Code Cleanup

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 // In App.js in a new project
 
-import * as React from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import WelcomeScreen from './src/screen/WelcomeScreen';
 import Navigator from './src/navigator';
+import firebase from 'react-native-firebase';
+
 export default class App extends React.Component {
   render() {
     return <Navigator />;
