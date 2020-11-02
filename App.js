efa2b39564543d7f049e1288e029b22cc657ea5b
Sopanat <sopanus2014@gmail.com>
2020-11-02 11:51:02 +0700

    feat: Initial FCM Device token registered..

--- App.js
+++ App.js
@@ -8,15 +8,45 @@ import {
   getManufacturer,
   getApplicationName,
   getDeviceName,
+  getAndroidId,
+  getLastUpdateTime,
 } from 'react-native-device-info';
 import RNLocation from 'react-native-location';
-
 import * as Sentry from '@sentry/react-native';
+import firestore from '@react-native-firebase/firestore';
 Sentry.init({
   dsn:
     'https://d426d2cc424e4a1e88180fe4b61b629d@o449610.ingest.sentry.io/5432874',
   enableNative: false,
 });
+
+async function saveTokenToDatabase(token) {
+  // Assume user is already signed in
+  const userId = await getAndroidId();
+
+  // Add the token to the users datastore
+  try {
+    await firestore()
+      .collection('Users')
+      .doc(userId)
+      .update({
+        tokens: firestore.FieldValue.arrayUnion(token),
+        userId: userId,
+      });
+  } catch {
+    firestore()
+      .collection('Users')
+      .doc(userId)
+      .set({
+        tokens: firestore.FieldValue.arrayUnion(token),
+        userId: userId,
+      })
+      .then(() => {
+        console.log('Firestore Device Registered!');
+      });
+  }
+}
+
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   console.log('Message handled in the background!', remoteMessage);
 });
@@ -37,9 +67,13 @@ export default class App extends React.Component {
     await this.setPermission();
     const fcmToken = await messaging().getToken();
     console.log('fcmToken', fcmToken);
-
     await messaging().registerDeviceForRemoteMessages();
+    await saveTokenToDatabase(fcmToken);
+    await messaging().onTokenRefresh((fcmToken) => {
+      saveTokenToDatabase(fcmToken);
+    });
   };
+
   setPermission = async () => {
     try {
       const enabled = await messaging().hasPermission();
@@ -50,6 +84,7 @@ export default class App extends React.Component {
       console.log('error', error);
     }
   };
+
   render() {
     return <Navigator />;
   }
