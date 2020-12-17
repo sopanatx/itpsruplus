89982f18fcba851b6833786ae4eb386fd349977a
sopanatx <sopanus2014@gmail.com>
2020-12-17 22:20:30 +0700

    feat (assets) : renaming assets file.

--- src/components/headerBar.js
+++ src/components/headerBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text, Image} from 'react-native';
+import {View, StyleSheet, Text, Image, ImageBackground} from 'react-native';
 
 import LinearGradient from 'react-native-linear-gradient';
 import {THEME} from '../styles';
@@ -8,18 +8,9 @@ import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 export const HeaderBar = () => {
   return (
     <View style={styles.header}>
-      <LinearGradient
-        start={{x: 0, y: 1.2}}
-        end={{x: 1, y: 0}}
-        colors={[THEME.WINTER_HEADER_1, THEME.WINTER_HEADER_2]}
-        style={{
-          shadowColor: 'rgba(245, 44, 80, 0.38)',
-          width: wp('100%'),
-          height: 165,
-          alignSelf: 'auto',
-          borderBottomRightRadius: 100,
-          backgroundColor: 'transparent',
-        }}>
+      <ImageBackground
+        source={require('../assets/images/HeadImage_3x.png')}
+        style={styles.image}>
         <Image
           style={styles.Logo}
           source={require('../assets/images/WhiteLogo_4x.png')}
@@ -28,13 +19,12 @@ export const HeaderBar = () => {
           style={{
             color: 'white',
             fontSize: 24,
-            textAlign: 'center',
-            //paddingVertical: -10,
+            justifyContent: 'center',
             fontFamily: 'DBHelvethaicaX-Bd',
           }}>
           Infomation Technology {'\n'}PSRU
         </Text>
-      </LinearGradient>
+      </ImageBackground>
     </View>
   );
 };
@@ -70,5 +60,10 @@ const styles = StyleSheet.create({
     color: '#f2f2f2',
     paddingVertical: 50,
     padding: 30,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
 });
