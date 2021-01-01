8982b48af81db7e2490f3ee4bd390df0609119fd
sopanatx <sopanus2014@gmail.com>
2021-01-01 13:08:59 +0700

    feat : Initial Firebase on iOS.

--- ios/itpsruplus/AppDelegate.m
+++ ios/itpsruplus/AppDelegate.m
@@ -26,11 +26,15 @@ static void InitializeFlipper(UIApplication *application) {
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+
+
 {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-
+  if ([FIRApp defaultApp] == nil) {
+      [FIRApp configure];
+    }
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"itpsruplus"
