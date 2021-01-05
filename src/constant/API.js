06e8ceec74c3dd80b8471168f176ad7e38970885
sopanatx <sopanus2014@gmail.com>
2021-01-05 22:47:04 +0700

    refactor api url

--- src/constant/API.js
+++ src/constant/API.js
@@ -1,12 +1,17 @@
-const TEST_URL = `https://api.itpsru.in.th`;
+const PRODUCTION_URL = `https://api.itpsru.in.th`;
+const TEST_URL = `https://test-api.itpsru.in.th`;
+
 export const USER_DATA_API_KEY = 'et6wg8uu7qw5b49xjte8mf3uw'; //API Key สำหรับ user data
 export const PUBLIC_API_KEY = 'baha9gwd2tk4ebqr6htwbu7pe'; //API Key สำหรับ Query ผลการเรียน
 export const APP_KEY = 'demr3exhwje76y8a';
-export const TEST_API_URL = {
-  auth: `${TEST_URL}auth/login`,
-  register: `${TEST_URL}/auth/login`,
-  calendar: `${TEST_URL}/student/calendar`,
+
+export const PRODUCTION_API = {
+  getversion: `${PRODUCTION_URL}/appversion`,
+  auth: `${PRODUCTION_URL}/auth/login`,
+  register: `${PRODUCTION_URL}/auth/register`,
+  calendar: `${PRODUCTION_URL}/student/calendar`,
 };
+
 export const PUBLIC_API = {
   grade: 'https://app.itpsru.in.th/api/grade',
 };
